d6191551ff7cf7ab3913a0020e7539f8b09345de
MTKtaha1 <129999518+MTKtaha1@users.noreply.github.com>
2024-01-25 19:53:27 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -13,7 +13,7 @@ class Assignment2:
         anniversary = [i for i in range(10, current_year - self.year + 1, 10)]
         return anniversary
     
-    def modify_year(self, n):
+    def modifyYear(self, n):
         year_str = str(self.year)
         repeated_prefix = year_str[:2] * n
         modified_year = self.year * n
